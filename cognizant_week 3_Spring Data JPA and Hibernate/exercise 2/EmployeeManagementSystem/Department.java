7e56e2cf496680c463dab3a1e2efaec858507759
trisha-rc <146533710+trisha-rc@users.noreply.github.com>
2024-08-14 15:36:49 +0530

    Add files via upload

---
+++ cognizant_week 3_Spring Data JPA and Hibernate/exercise 2/EmployeeManagementSystem/Department.java
@@ -0,0 +1,42 @@
+package com.example.employeemanagementsystem.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
