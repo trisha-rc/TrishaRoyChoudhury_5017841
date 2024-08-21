f553647a3af24337ab22d7a343b253f484f1bff4
trisha-rc <146533710+trisha-rc@users.noreply.github.com>
2024-08-21 21:28:50 +0530

    Add files via upload

---
+++ cognizant_week 4,5_Spring REST/exercise 4/BookstoreApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bookstore.bookstoreapi_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
