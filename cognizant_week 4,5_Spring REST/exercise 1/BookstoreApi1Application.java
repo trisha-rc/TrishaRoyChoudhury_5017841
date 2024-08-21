2580f9046f7f348d56fe402717cd21addcc13b74
trisha-rc <146533710+trisha-rc@users.noreply.github.com>
2024-08-21 21:20:03 +0530

    Add files via upload

---
+++ cognizant_week 4,5_Spring REST/exercise 1/BookstoreApi1Application.java
@@ -0,0 +1,12 @@
+package bookstore.bookstoreapi_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApi1Application.class, args);
+    }
+}
